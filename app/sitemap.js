04cb12272efb1f6095642eb4a309cb2dd4725434
Dominik <dominik.jojczyk@gmail.com>
2024-02-03 23:36:14 +0100

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://dominikjojczyk.pl",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
